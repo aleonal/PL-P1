41ee37f2865d1f3966d16bfd3664a58d4572c94e
aleonal <jaleoncordero@miners.uetp.edu>
2018-09-30 17:26:20 -0600

    Added several classes to play/index.php

--- src/info/index.php
+++ src/info/index.php
@@ -12,5 +12,7 @@
         }
     }
 
+    //Import data from game state saved in writable using PID. Generic PID -> 00000000
+
     $info = new GameInfo(6, 7, array("Smart" => "SmartStrategy", "Random" => "RandomStrategy"));
     echo json_encode($info);
