036db989af185a3447d0d77f4b82e21b76e0fedb
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2018-10-01 14:07:40 -0600

    Persisting Game State
    Initialized the persisting game state function to be used for continuous play, saved in an external file as specified by the lab instructions

--- src/play/index.php
+++ src/play/index.php
@@ -1,1 +1,17 @@
 <?php
+
+//...
+//$file = ... $pid ...;
+$json = file_get_contents($file);
+$game = Game::fromJsonString($json);
+$playerMove = $game->makePlayerMove($x, $y);
+if ($playerMove->isWin || $playerMove->isDraw) {
+    unlink($file);
+    exit();
+}
+$opponentMove = $game->makeOpponentMove($x, $y);
+if ($opponentMove->isWin || $opponentMove->isDraw) {
+    unlink($file);
+    exit();
+}
+storeState($file, $game->toJsonString());
