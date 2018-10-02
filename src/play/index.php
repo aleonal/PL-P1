b440a72d559f5d2083a908fd741b815c58acaab3
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2018-10-02 17:04:57 -0600

    Response of play/index
    Added the sample echoing of the play/index.php file, it just serves as orientation

--- src/play/index.php
+++ src/play/index.php
@@ -21,3 +21,34 @@ class invalidResponse {
     }
 }
 
+//Once conditions are satisfied continue
+
+$playerMove = makePlayerMove($slot);
+if ($playerMove->isWin || $playerMove->isDraw) {
+    echo createResponse($playerMove);
+    exit();
+}
+
+$opponentMove = makeOpponentMove();
+echo createResponse($playerMove, $opponentMove);
+
+function createResponse($playerMove, $opponentMove = null)
+{
+    $result = array(
+        "response" => true,
+        "ack_move" => $playerMove
+    );
+    if ($opponentMove != null) {
+        $result["move"] = $opponentMove;
+    }
+    return json_encode($result);
+}
+
+class invalidResponse {
+    var $response = false;
+    var $reason;
+    
+    function __construct($reason) {
+        $this->reason = $reason;
+    }
+}
