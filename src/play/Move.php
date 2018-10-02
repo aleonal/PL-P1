e35fd04c730b834161d70966733cf15095549ed9
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2018-10-02 15:44:59 -0600

    Completed -aux method
    What this method does is check in all possible directions from the source of the dropped disc and determines if 4 of the same player are lined up in a valid winning possition

--- src/play/Move.php
+++ src/play/Move.php
@@ -9,8 +9,7 @@ class Move {
     var $isDraw;
     var $win;
 
-	 function isWin($type)
-    {
+	 function isWin($type){
         $x = $playerMove->slot;
         $y = 5;
         $notDone = 1;
@@ -25,27 +24,91 @@ class Move {
         return done;
     }
 
-	 private function aux($dx, $dy, $pendingName, $ind)
-    {
+   private function aux($dx, $dy, $pendingName, $ind) {
         $count = 0;
-        while ($dx > 0 || $dy < 5) {
-            $dx --;
-            $dy ++;
-        }
-        while ($dx < 6 || $dy > 0) {
-            if (Matrix[$dx][$dy] == $pendingName) {
-                $count ++;
-            } else {
-                $count = 0;
-            }
-        }
-        if (count >= 4) {
-            setWinArray($dx, $dy, "NW");
-            return 1;
+        switch ($ind) {
+            case 1:
+                while ($dx > 0 || $dy < 5) {
+                    $dx --;
+                    $dy ++;
+                }
+                while ($dx < 6 || $dy > 0) {
+                    if (Matrix[$dx][$dy] == $pendingName) {
+                        $count ++;
+                    } else {
+                        $count = 0;
+                    }
+                    $dx ++;
+                    $dy --;
+                }
+                if (count >= 4) {
+                    setWinArray($dx, $dy, "NW");
+                    return 1;
+                }
+                break;
+            case 2:
+                while ($dx > 0) {
+                    $dx --;
+                }
+                while ($dx < 6) {
+                    if (Matrix[$dx][$dy] == $pendingName) {
+                        $count ++;
+                    } else {
+                        $count = 0;
+                    }
+                    $dx ++;
+                }
+                if (count >= 4) {
+                    setWinArray($dx, $dy, "W");
+                    return 1;
+                }
+                break;            
+            case 3:
+                while ($dx > 0 || $dy > 0) {
+                    $dx --;
+                    $dy --;
+                }
+                while ($dx < 6 || $dy < 5) {
+                    if (Matrix[$dx][$dy] == $pendingName) {
+                        $count ++;
+                    } else {
+                        $count = 0;
+                    }
+                    $dx ++;
+                    $dy ++;
+                }
+                if (count >= 4) {
+                    setWinArray($dx, $dy, "SW");
+                    return 1;
+                }
+                break;            
+            case 4:
+                while ($dy > 0) {
+                    $dy --;
+                }
+                while ($dy < 5) {
+                    if (Matrix[$dx][$dy] == $pendingName) {
+                        $count ++;
+                    } else {
+                        $count = 0;
+                    }
+                    $dy ++;
+                }
+                if (count >= 4) {
+                    setWinArray($dx, $dy, "S");
+                    return 1;
+                }
+                break;
+            default:
+                return 0;
         }
         return 0;
     }
-} 
+}
+
+	
+
+
 /*
 $playerMove = makePlayerMove($slot);
 if ($playerMove->isWin || $playerMove->isDraw) {
