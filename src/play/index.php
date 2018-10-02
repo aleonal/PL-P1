42a235264f71bfeaedf1fce6d31b41a31916a7b3
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2018-10-02 15:24:53 -0600

    Required request for move
    Introduced the error conditions if the input move results invalid

--- src/play/index.php
+++ src/play/index.php
@@ -1,17 +1,23 @@
 <?php
 
-//...
-//$file = ... $pid ...;
-$json = file_get_contents($file);
-$game = Game::fromJsonString($json);
-$playerMove = $game->makePlayerMove($x, $y);
-if ($playerMove->isWin || $playerMove->isDraw) {
-    unlink($file);
-    exit();
+//insert the get the pid conditionals here
+
+if ($_GET["move"] == "") {
+    echo json_encode(new invalidResponse("Move not specified"));
+    exit;
 }
-$opponentMove = $game->makeOpponentMove($x, $y);
-if ($opponentMove->isWin || $opponentMove->isDraw) {
-    unlink($file);
-    exit();
+//check if the move is outside boundaries: 0<move<6 (inclusive)
+elseif($_GET["move"] < 0 || $_GET["move"] > 6) {
+    echo json_encode(new invalidResponse("Invalid slot, " . $_GET["move"]));
+    exit;
+} 
+
+class invalidResponse {
+    var $response = false;
+    var $reason;
+    
+    function __construct($reason) {
+        $this->reason = $reason;
+    }
 }
-storeState($file, $game->toJsonString());
+
