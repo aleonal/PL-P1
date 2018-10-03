42ee2fe157a515382549cfe39b8f4e90ca1154a3
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2018-10-03 13:41:50 -0600

    Deleted uneeded code
    Deleted some code that belonged to the index.php file of the play directory

--- src/play/Move.php
+++ src/play/Move.php
@@ -143,28 +143,3 @@ class Move {
 }
 
 }
-
-	
-
-
-/*
-$playerMove = makePlayerMove($slot);
-if ($playerMove->isWin || $playerMove->isDraw) {
-    echo createResponse($playerMove);
-    exit();
-}
-$opponentMove = makeOpponentMove();
-echo createResponse($playerMove, $opponentMove);
-
-function createResponse($playerMove, $opponentMove = null)
-{
-    $result = array(
-        "response" => true,
-        "ack_move" => $playerMove
-    );
-    if ($opponentMove != null) {
-        $result["move"] = $opponentMove;
-    }
-    return json_encode($result);
-}
-*/
