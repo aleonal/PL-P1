84e5073a1e6810baba397abb6046f94a1497ba3e
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2018-10-03 14:00:46 -0600

    Added default Board inst.
    The default board construct will make an empty board to be used by the game class

--- src/play/Board.php
+++ src/play/Board.php
@@ -4,5 +4,10 @@ class Board {
 
 	function __construct(){
 	$this->matrix = array(array());	
+	for ($i = 0; $i < 6; $i++) {
+            for ($j = 0; $j < 7; $j++) {
+                $this->matrix[i][j] = 0;
+            }
+        }
 	}
 }
