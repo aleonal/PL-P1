7bcb07aafe2886bf60e078a83624520dc0df8a60
aleonal <jaleoncordero@miners.utep.edu>
2018-10-08 21:01:43 -0600

    Fixed width and height in info

--- src/info/index.php
+++ src/info/index.php
@@ -1,7 +1,7 @@
 <?php
     require '../globals/globals.php';
 
-    $info = new GameInfo(6, 7, $strategy);
+    $info = new GameInfo(7, 6, $strategy);
     echo json_encode($info);
 
 class GameInfo {
