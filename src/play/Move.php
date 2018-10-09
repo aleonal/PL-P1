da5126fdd86b69915453d20a44010f838edfb293
Ricardo Sanchez <rasanchez7@miners.utep.edu>
2018-10-08 19:28:47 -0600

    Updated Move(Strategy).php
    Updated both classes Move and MoveStrategy to a semifinal version

--- src/play/Move.php
+++ src/play/Move.php
@@ -1,145 +1,166 @@
 <?php
-require_once '../globals/globals.php';
-require_once '../play/Game.php';
-require_once '../play/FileIO.php';
-
-class Move {
-    var $slot;
-    var $isWin;
-    var $isDraw;
-    var $win;
-
-	 function isWin($type){
-        $x = $playerMove->slot;
-        $y = 5;
-        $notDone = 1;
-        while (Matrix[$x][$y] === 0) {
-            $y --;
+	require_once '../play/Game.php';
+        
+    class Move {
+        var $slot;
+        var $isWin;
+        var $isDraw;
+        var $win;
+        
+        
+        function __construct(){
+            $this->slot = -1;
+            $this->isWin = FALSE;
+            $this->isDraw = FALSE;
+            $this->row = array(array());
         }
-        $i = 0;
-        while ($notDone || i < 4) {
-            $notDone = aux($x, $y, $type, $i);
-            $i++;
+        
+        function __construct($val, &$board){
+            $this->row = array(array());
+            $this->slot = $val;
+            $this->isWin = isWin($board,1);
+            $this->isDraw = isDraw();
         }
-        return done;
-    }
-
-   private function aux($dx, $dy, $pendingName, $ind) {
-        $count = 0;
-        switch ($ind) {
-            case 1:
-                while ($dx > 0 || $dy < 5) {
-                    $dx --;
-                    $dy ++;
-                }
-                while ($dx < 6 || $dy > 0) {
-                    if (Matrix[$dx][$dy] == $pendingName) {
-                        $count ++;
-                    } else {
-                        $count = 0;
-                    }
-                    $dx ++;
-                    $dy --;
-                }
-                if (count >= 4) {
-                    setWinArray($dx, $dy, "NW");
-                    return 1;
-                }
-                break;
-            case 2:
-                while ($dx > 0) {
-                    $dx --;
-                }
-                while ($dx < 6) {
-                    if (Matrix[$dx][$dy] == $pendingName) {
-                        $count ++;
-                    } else {
-                        $count = 0;
-                    }
-                    $dx ++;
-                }
-                if (count >= 4) {
-                    setWinArray($dx, $dy, "W");
-                    return 1;
-                }
-                break;            
-            case 3:
-                while ($dx > 0 || $dy > 0) {
-                    $dx --;
-                    $dy --;
-                }
-                while ($dx < 6 || $dy < 5) {
-                    if (Matrix[$dx][$dy] == $pendingName) {
-                        $count ++;
-                    } else {
-                        $count = 0;
-                    }
-                    $dx ++;
-                    $dy ++;
-                }
-                if (count >= 4) {
-                    setWinArray($dx, $dy, "SW");
-                    return 1;
-                }
-                break;            
-            case 4:
-                while ($dy > 0) {
-                    $dy --;
-                }
-                while ($dy < 5) {
-                    if (Matrix[$dx][$dy] == $pendingName) {
-                        $count ++;
-                    } else {
-                        $count = 0;
-                    }
-                    $dy ++;
-                }
-                if (count >= 4) {
-                    setWinArray($dx, $dy, "S");
-                    return 1;
-                }
-                break;
-            default:
-                return 0;
+        
+        function __construct($val, &$board, $cond){
+            $this->row = array(array());
+            $this->slot = $val;
+            $this->isWin = isWin($board,-1);
+            $this->isDraw = isDraw();
         }
-        return 0;
+        
+        function isWin(&$board,$type){
+            $x = $playerMove->slot;
+            $y = 5;
+            $notDone = 1;
+            while ($Matrix[$x][$y] == 0) {
+                $y --;
+            }
+            $i = 0;
+            while ($notDone || i < 4) {
+                $notDone = aux($x, $y, $type, $i);
+                $i++;
+            }
+            return $notDone;
+        }
+        
+        private function aux($dx, $dy, $pendingName, $ind) {
+            $count = 0;
+            switch ($ind) {
+                case 1:
+                    while ($dx > 0 || $dy < 5) {
+                        $dx --;
+                        $dy ++;
+                    }
+                    while ($dx < 6 || $dy > 0) {
+                        if ($Matrix[$dx][$dy] == $pendingName) {
+                            $count ++;
+                        } else {
+                            $count = 0;
+                        }
+                        $dx ++;
+                        $dy --;
+                    }
+                    if ($count >= 4) {
+                        setWinArray($dx, $dy, "NW");
+                        return TRUE;
+                    }
+                    break;
+                case 2:
+                    while ($dx > 0) {
+                        $dx --;
+                    }
+                    while ($dx < 6) {
+                        if ($Matrix[$dx][$dy] == $pendingName) {
+                            $count ++;
+                        } else {
+                            $count = 0;
+                        }
+                        $dx ++;
+                    }
+                    if ($count >= 4) {
+                        setWinArray($dx, $dy, "W");
+                        return TRUE;
+                    }
+                    break;
+                case 3:
+                    while ($dx > 0 || $dy > 0) {
+                        $dx --;
+                        $dy --;
+                    }
+                    while ($dx < 6 || $dy < 5) {
+                        if ($Matrix[$dx][$dy] == $pendingName) {
+                            $count ++;
+                        } else {
+                            $count = 0;
+                        }
+                        $dx ++;
+                        $dy ++;
+                    }
+                    if ($count >= 4) {
+                        setWinArray($dx, $dy, "SW");
+                        return TRUE;
+                    }
+                    break;
+                case 4:
+                    while ($dy > 0) {
+                        $dy --;
+                    }
+                    while ($dy < 5) {
+                        if ($Matrix[$dx][$dy] == $pendingName) {
+                            $count ++;
+                        } else {
+                            $count = 0;
+                        }
+                        $dy ++;
+                    }
+                    if ($count >= 4) {
+                        setWinArray($dx, $dy, "S");
+                        return TRUE;
+                    }
+                    break;
+                default:
+                    return FALSE;
+            }
+            return FALSE;
+        }
+        
+        function setWinArray($x, $y, $direction){
+            switch ($direction) {
+                case "NW":
+                    for ($i = 3; $i >= 0; $i--) {
+                        $win[$i * 2] = $x;
+                        $win[$i * 2 + 1] = $y;
+                        $y--;
+                        $x++;
+                    }
+                    break;
+                case "W":
+                    for ($i = 3; $i >= 0; $i--) {
+                        $win[$i * 2] = $x;
+                        $win[$i * 2 + 1] = $y;
+                        $x++;
+                    }
+                    break;
+                case "SW":
+                    for ($i = 3; $i >= 0; $i--) {
+                        $win[$i * 2] = $x;
+                        $win[$i * 2 + 1] = $y;
+                        $x++;
+                        $y++;
+                    }
+                    break;
+                case "S":
+                    for ($i = 3; $i >= 0; $i--) {
+                        $win[$i * 2] = $x;
+                        $win[$i * 2 + 1] = $y;
+                        $y++;
+                    }
+                    break;
+                default:
+                    return 0;
+            }
+        }
+        
     }
-
-	function setWinArray($x, $y, $direction){
-    switch ($direction) {        
-        case "NW":
-            for ($i = 3; $i >= 0; $i--) {
-                $win[$i * 2] = $x;
-                $win[$i * 2 + 1] = $y;
-                $y--;
-                $x++;
-            }
-            break;
-        case "W":
-            for ($i = 3; $i >= 0; $i--) {
-                $win[$i * 2] = $x;
-                $win[$i * 2 + 1] = $y;                
-                $x++;
-            }
-            break;
-        case "SW":
-            for ($i = 3; $i >= 0; $i--) {
-                $win[$i * 2] = $x;
-                $win[$i * 2 + 1] = $y;
-                $x++;
-                $y++;
-            }
-            break;
-        case "S":
-            for ($i = 3; $i >= 0; $i--) {
-                $win[$i * 2] = $x;
-                $win[$i * 2 + 1] = $y;
-                $y++;
-            }
-            break;
-        default:
-            return 0;
-    }
-}
-
-}
+    
--- src/play/MoveStrategy.php
+++ src/play/MoveStrategy.php
@@ -1,11 +1,20 @@
 <?php
+require_once '../play/Move.php';
 
-function smartStrategy($board) {
-    $slot = //result from calculations
-    return new Move($slot);
+function smartStrategy(&$board){
+    $tempMatrix = $board;
+    $tempMove = new Move();
+    for ($i = 0; $tempMove->isWin != FALSE; $i ++) {
+        $tempMove = new Move($i, $board);
+    }
+    $slot = $tempMove->slot;
+    if($slot == -1){
+        $slot = mt_rand(2,4);
+    }
+    return new Move($slot, $board, TRUE);
 }
 
-function randomStrategy($board) {
-    $slot = //result from RNG
-    return new Move($slot);
+function randomStrategy(&$board){
+    $slot = mt_rand(0, 6);
+    return new Move($slot, $board, TRUE);
 }
